cfd4ad50f3d984737c5c441ca25f5b0eb1271ba9
Nguyen Van Truong <ttntruong79@gamil.com>
2023-04-13 00:20:30 +0700

    update test notification

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 const secondBox = document.querySelector('#seconds'),
     tensBox = document.querySelector('#tens'),
-    btnStart = document.querySelector('#start'),
+    minuteBox = document.querySelector('#minute')
     btnStopContinue = document.querySelector('#stop-continue'),
     bntReset = document.querySelector('#reset');
 
-    console.log(btnStart,btnStopContinue,bntReset )
+const alertTime = 2;
 
 var currentTime = {
     second: 0,
     tens: 0,
+    minute: 0,
     isRunning: false
 }
 
 var interval;
 
+setInterval(notification,1000)
+
+function notification(){
+    if(alertTime === currentTime.minute*60 + currentTime.second){
+        Notification.requestPermission().then(perm=>{
+            // alert(perm)
+            if(perm === 'granted'){
+                const noti = new Notification("Complete this task",{
+                    body: "This is task notification!!",
+                    data: {
+                        hello: 'world'
+                    },
+                })
+                console.log(noti)
+                noti.addEventListener('close',(event)=>{
+                    console.log(event)
+                })
+                
+            }
+        })
+    }
+}
+
 function showTime(){
     secondBox.textContent = String(currentTime.second).padStart(2,'0');
     tensBox.textContent = String(currentTime.tens).padStart(2,'0');
+    minuteBox.textContent = String(currentTime.minute).padStart(2,'0')
 }
 
 bntReset.addEventListener('click',()=>{
@@ -24,19 +49,8 @@ bntReset.addEventListener('click',()=>{
     currentTime.second = 0;
     currentTime.tens = 0;
     currentTime.isRunning = false;
+    btnStopContinue.classList.remove('running')
     showTime();
-})
-
-btnStart.addEventListener('click',()=>{
-    if(currentTime.isRunning){
-        alert('Watch is running!!!')
-        return;
-    }
-    else{
-        interval = setInterval(runTime,10);
-        currentTime.isRunning = true;
-        btnStopContinue.classList.add('running')
-    }
 })
 
 btnStopContinue.addEventListener('click',()=>{
@@ -57,6 +71,10 @@ const runTime = ()=>{
     if(currentTime.tens>=100){
         currentTime.tens = 0;
         currentTime.second++
+        if(currentTime.second == 60){
+            currentTime.second = 0;
+            currentTime.minute++;
+        }
     }
     showTime();
 }
